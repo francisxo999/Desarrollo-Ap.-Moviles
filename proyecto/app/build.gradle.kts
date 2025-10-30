d81e1b5a96ee7bb2a94b77a3d0cc27b70f61bb7d
francisxo999 <fra.verab@duocuc.cl>
2025-10-30 20:49:24 -0300

    feat: configurar e integrar Firebase Auth y Firestore al proyecto

--- proyecto/app/build.gradle.kts
+++ proyecto/app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.google.gms.plugin)
 }
 
 android {
@@ -53,25 +55,23 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.google.material) // Tema base para Manifest
+    implementation(libs.google.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
     implementation(libs.androidx.material.icons.extended)
-
     implementation(libs.androidx.navigation.compose)
-
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
